d3cce84518ff882f32f5b2986d33a6499c94c3fb
Kevin <kevinrsa@gmail.com>
2020-01-17 16:53:35 +0000

    First pass

---
+++ src/StockService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using ThreeFourteen.QuickService;
+
+namespace StockService
+{
+    class Program
+    {
+        public static Task Main(string[] args)
+        {
+            var service = new ServiceHost();
+            return service.Run<Startup>(args);
+        }
+    }
+}
---
+++ src/StockService/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using ThreeFourteen.QuickService;
+
+namespace StockService
+{
+    public class Startup : ServiceStartup
+    {
+        public Startup(IConfiguration configuration) 
+            : base(configuration)
+        {
+        }
+    }
+}
